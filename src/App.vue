8260cd48363ff3af04ea69e96dee19e9d6cd19c3
vncscoelho <vncs.coelho@gmail.com>
2021-05-04 18:31:09 -0300

    feat: add base input

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <template>
   <div id="app">
-
+    <page-header />
   </div>
 </template>
+<script>
+import API from '@/api';
 
+export default {
+  components: {
+    PageHeader: () => import('@/components/PageHeader.vue'),
+  },
+  created() {
+    const instance = new API();
+    console.log(instance);
+  },
+};
+</script>
 <style lang="scss">
-
+body {
+  font-family: sans-serif;
+}
 </style>
---
+++ src/components/UI/BaseInput.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="base-input">
+    <span
+      v-if="icon"
+      class="base-input__icon"
+    >
+      {{ icon }}
+    </span>
+    <input
+      type="text"
+      class="base-input__element"
+      v-bind="$attrs"
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    icon: {
+      type: String,
+      default: null,
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.base-input {
+  display: flex;
+  align-items: center;
+  width: 100%;
+  padding: 0.75em 1em;
+  border: $border;
+  border-radius: $radius;
+  background: #fff;
+  &__element {
+    outline: none;
+    border: 0;
+    background: none;
+  }
+  &__icon {
+    & + input {
+      padding-left: 0.75em;
+    }
+  }
+}
+</style>
