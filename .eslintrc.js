afdb0845df0862c01414bd391e76f9839543b657
vncscoelho <vncs.coelho@gmail.com>
2021-05-04 17:38:05 -0300

    fix: eslint issue with svg attrs

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,23 +3,27 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-  ],
+  extends: ['plugin:vue/essential', '@vue/airbnb'],
   parserOptions: {
     parser: 'babel-eslint',
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'max-len': 'off',
+    'vue/max-len': [
+      'error',
+      {
+        code: 100,
+        template: 100,
+        ignoreHTMLAttributeValues: true,
+        ignoreHTMLTextContents: true,
+      },
+    ],
   },
   overrides: [
     {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)',
-      ],
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
       env: {
         jest: true,
       },
