57c5de96f9ab5adc713b3d522edd0c10bcbc2187
vncscoelho <vncs.coelho@gmail.com>
2021-05-04 20:42:30 -0300

    feat: add base loader

---
+++ src/components/UI/BaseLoader.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="base-loader">
+    <page-logo no-logotype />
+  </div>
+</template>
+
+<script>
+import PageLogo from '@/components/PageLogo.vue';
+
+export default {
+  components: {
+    PageLogo,
+  },
+};
+</script>
+
+<style lang="scss">
+.base-loader {
+  width: 33px;
+  height: 32px;
+  overflow: hidden;
+  animation: spin 2s infinite linear;
+  .page-logo {
+    height: 32px;
+  }
+  @keyframes spin {
+    from {
+      transform: rotate(-180deg);
+    }
+    to {
+      transform: rotate(180deg);
+    }
+  }
+}
+</style>
