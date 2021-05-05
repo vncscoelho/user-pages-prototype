d9268bd501ee14af2799f010f5c8988b9440b907
vncscoelho <vncs.coelho@gmail.com>
2021-05-05 00:02:47 -0300

    feat: move header to app

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
 <template>
   <div id="app">
+    <page-header />
     <router-view />
   </div>
 </template>
+
+<script>
+export default {
+  components: {
+    PageHeader: () => import('@/components/PageHeader.vue'),
+  },
+};
+</script>
 
 <style lang="scss">
 body {
@@ -15,5 +24,26 @@ body {
 }
 .spaced {
   padding: 1em 1.25em;
+}
+.space-between {
+  display: flex;
+  justify-content: space-between;
+}
+.page {
+  padding: 2em 0;
+}
+.fade {
+  &-leave-active,
+  &-enter-active {
+    transition: 200ms ease-in-out;
+  }
+  &-leave,
+  &-enter-to {
+    opacity: 1;
+  }
+  &-leave-to,
+  &-enter {
+    opacity: 0;
+  }
 }
 </style>
