c00b646740248a82713bd49a597160869a99b2b2
vncscoelho <vncs.coelho@gmail.com>
2021-05-05 01:39:04 -0300

    feat: add footer

---
+++ src/components/PageFooter.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer class="page-footer">
+    <div class="container">
+      <page-logo white />
+    </div>
+  </footer>
+</template>
+
+<script>
+import PageLogo from '@/components/PageLogo.vue';
+
+export default {
+  components: { PageLogo },
+};
+</script>
+
+<style lang="scss">
+.page-footer {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 3em 0;
+  background: $text;
+}
+</style>
